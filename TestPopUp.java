c05638bb0da48fe905d64cfcac5de9959bcfd099
Sarah Walker <sarah.walker@lakesideschool.org>
2015-05-18 10:29:46 -0700

    first commit

---
+++ TestPopUp.java
@@ -0,0 +1,13 @@
+//Sarah 
+//tester program 
+
+import javax.swing.*;
+
+public class TestPopUp
+{
+   public static void main(String args[])
+   {
+      ////x.createDialog("HI SARAH!!!!!!!!");
+      JOptionPane.showMessageDialog(null, "HI SARAH");
+   }
+}
